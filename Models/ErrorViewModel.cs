5d4456963c1a8f32843c390d3c7704b4b21e5c9f
Kamal Singh <kamaletah@gmail.com>
2024-10-03 16:07:06 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TextDemoGithub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
